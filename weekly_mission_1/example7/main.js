fab9fa923fc754b9cfb8cff79f8cb6aca78022d7
Danny Veneno <yehiibhiidigital@gmail.com>
2022-04-10 03:52:31 -0500

    se modifico carpetas y documentos

--- weekly_mission_1/example7/main.js
+++ weekly_mission_1/example7/main.js
@@ -1,3 +1,18 @@
-import * as loggerModule from './logger.js'
+const Pokemon = require('./pokemon')
 
-console.log(loggerModule)
+const pikachu = new Pokemon("pikachu")
+const bulbasaur = new Pokemon("bulbasaur")
+const squirtle = new Pokemon("squirtle")
+const charmander = new Pokemon("charmander")
+
+pikachu.sayHello()
+pikachu.sayMessage("Heey!")
+
+bulbasaur.sayHello()
+bulbasaur.sayMessage("Heey!")
+
+charmander.sayHello()
+charmander.sayMessage("Heey!")
+
+squirtle.sayHello()
+squirtle.sayMessage("Heey!")
